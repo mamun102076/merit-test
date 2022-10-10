38bb63bb9807538691c54952c4cb19cf77ed0cf9
mamun102076 <mamun102076@gmail.com>
2022-10-11 04:48:56 +0600

    added header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,8 +3,8 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='header'>
-            <h2>Merit Test</h2>
-            <p>Where we verify knowldge</p>
+            <h2>Our goal is to test the merit</h2>
+            <p>Here we verify knowldge.Our goal is to justify knowldge.<br></br> Process of managing and implementing our goals and objectives.<br></br>We ensure students understands the question and requirements and answer properly.</p>
             <p></p>
         </div>
     );
